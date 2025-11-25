e9a4efb479d97a140784dc1a3367e372143a6ff8
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-25 14:45:08 -0300

    Integration tests for persistence

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -3,4 +3,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE courierdb;
 	CREATE DATABASE deliverydb;
+	CREATE DATABASE deliverydb_test;
 EOSQL
