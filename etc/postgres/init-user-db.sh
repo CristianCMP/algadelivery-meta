c23c301078d398972ad80d649b0cf9b08d302ed6
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-25 17:31:32 -0300

    Integration test with REST Assured.

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -3,5 +3,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE courierdb;
 	CREATE DATABASE deliverydb;
+	CREATE DATABASE courierdb_test;
 	CREATE DATABASE deliverydb_test;
 EOSQL
